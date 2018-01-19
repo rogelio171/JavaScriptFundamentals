312c614c134c1197a50bc80e038aa4674088c903
Rogelio Orona <rogelio171@gmail.com>
2018-01-18 20:53:51 -0600

    Add methods to objects example

--- distanceBetweenPoints.js
+++ distanceBetweenPoints.js
@@ -1,18 +1,52 @@
-const p1 = {
-    x: 0, 
-    y: 4
+// function Point(x, y) {
+//     this.x = x
+//     this.y = y
+// }
+
+// Point.prototype.moveX = function moveX(x) {
+//     this.x += x
+// }
+
+// Point.prototype.moveY = function moveY(y) {
+//     this.y += y
+// }
+
+// Point.prototype.distance = function distance(p) {
+//     const x = this.x - p.x
+//     const y = this.y - p.y
+
+//     return Math.sqrt( x * x + y * y)
+// }
+
+const Point = {
+    init: function(x, y) {
+        this.x = x
+        this.y = y
+    },
+    moveX: function moveX(x) {
+        this.x += x
+    },
+    moveY: function moveY(y) {
+        this.y += y
+    },
+    distance: function distance(p) {
+        const x = this.x - p.x
+        const y = this.y - p.y
+
+        return Math.sqrt( x * x + y * y)
+    }
 }
 
-const p2 = {
-    x: 3,
-    y: 0
-}
+//const p1 = new Point(0, 4)
+const p1 = Object.create(Point)
+p1.init(0, 4)
 
-function distance(p1, p2) {
-    const x = p1.x - p2.x
-    const y = p1.y - p2.y
-    
-    return Math.sqrt(x * x + y * y)
-}
+//const p2 = new Point(3, 0)
+const p2 = Object.create(Point)
+p2.init(3, 0)
 
-console.log(distance(p1, p2))
+console.log(p1.distance(p2))
+p1.moveX(10)
+console.log(p1.distance(p2))
+p2.moveY(-4)
+console.log(p1.distance(p2))
