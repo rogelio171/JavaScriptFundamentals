ce18ea1ae520c50786e2c15f032553f9cf96c880
Rogelio Orona <rogelio171@gmail.com>
2018-01-18 21:05:15 -0600

    Add class keyword to create objects

--- distanceBetweenPoints.js
+++ distanceBetweenPoints.js
@@ -1,49 +1,28 @@
-// function Point(x, y) {
-//     this.x = x
-//     this.y = y
-// }
-
-// Point.prototype.moveX = function moveX(x) {
-//     this.x += x
-// }
-
-// Point.prototype.moveY = function moveY(y) {
-//     this.y += y
-// }
-
-// Point.prototype.distance = function distance(p) {
-//     const x = this.x - p.x
-//     const y = this.y - p.y
-
-//     return Math.sqrt( x * x + y * y)
-// }
-
-const Point = {
-    init: function(x, y) {
+class Point {
+    constructor(x, y) {
         this.x = x
         this.y = y
-    },
-    moveX: function moveX(x) {
+    }
+
+    moveX(x) {
         this.x += x
-    },
-    moveY: function moveY(y) {
+    }
+
+    moveY(y) {
         this.y += y
-    },
-    distance: function distance(p) {
+    }
+
+    distance(p) {
         const x = this.x - p.x
         const y = this.y - p.y
-
+    
         return Math.sqrt( x * x + y * y)
     }
 }
 
-//const p1 = new Point(0, 4)
-const p1 = Object.create(Point)
-p1.init(0, 4)
+const p1 = new Point(0, 4)
 
-//const p2 = new Point(3, 0)
-const p2 = Object.create(Point)
-p2.init(3, 0)
+const p2 = new Point(3, 0)
 
 console.log(p1.distance(p2))
 p1.moveX(10)
