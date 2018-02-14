4c779baef15dc3bae70c0abd9f36cc4d24dd05bc
Rogelio Orona <Rogelio_Orona@epam.com>
2018-02-14 15:22:07 -0600

    Adds generator example

---
+++ fibonacciGenerator.js
@@ -0,0 +1,12 @@
+function* fibonacci() {
+    let a = 0,
+        b = 1
+
+    while (true) {
+        let f = a
+        a = b
+        b = f + a
+        let reset = yield f
+        if (reset) a = 0, b = 1
+    }
+}
